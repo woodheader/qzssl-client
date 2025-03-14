7a65f024a3273a35bcc78252e0e2ea06de124ef5
lishaojie <lishaojie@lishaojiedeMacBook-Pro-2.local>
2025-03-14 11:34:42 +0800

    add runtime logs

--- run_linux.sh
+++ run_linux.sh
@@ -4,7 +4,7 @@ if [ -z "$sslPids" ]
 then
 	echo 'Ssl resign procedure is empty.'
 	cd ./src/
-	sudo ../bin/linux/bin/node resign_server.js &
+	sudo ../bin/linux/bin/node resign_server.js > ../run.log 2>&1 &
 else
 	echo 'Ssl resign procedure is not empty'
 	ps aux|grep 'node resign_server.js'|grep -v 'grep'|awk '{print $2}'|xargs kill -9
